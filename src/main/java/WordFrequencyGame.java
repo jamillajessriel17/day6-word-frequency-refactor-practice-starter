b6e7773db4efbfa54721b4dd22094a589be03385
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-21 17:07:45 +0800

    refactor: remove unused code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,9 +3,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.io.CharArrayWriter;
-
-import java.time.LocalDateTime;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr){
@@ -56,7 +53,6 @@ public class WordFrequencyGame {
     private Map<String,List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
         for (Input input :  inputList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
             if (!map.containsKey(input.getValue())){
                 ArrayList arr = new ArrayList<>();
                 arr.add(input);
