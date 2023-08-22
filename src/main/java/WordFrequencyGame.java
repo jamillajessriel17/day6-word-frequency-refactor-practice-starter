da2692ed80a21274d5bf800d759edcd27532549a
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-22 10:34:22 +0800

    code review changes

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,38 +7,26 @@ public class WordFrequencyGame {
     public static final String NEW_LINE_DELIMITER = "\n";
     public static final String SPACE_CHAR = " ";
     public static final String CALCULATE_ERROR = "Calculate Error";
-    public static final String SPACE_ONE = " 1";
 
     public String getResult(String inputString) {
         List<String> words = splitWords(inputString);
 
-        if (hasSizeOf1(words)) {
-            return inputString + SPACE_ONE;
-        }
         try {
             List<WordFrequencyInfo> wordFrequencyInfoList = getWordFrequencyInfoList(words);
-            sort(wordFrequencyInfoList);
-            return generatePrintLines(wordFrequencyInfoList);
+            return generateSortedPrintLines(wordFrequencyInfoList);
         } catch (Exception e) {
             return CALCULATE_ERROR;
         }
-    }
-
-    private boolean hasSizeOf1(List<String> words) {
-        return words.size() == 1;
-    }
-
-    private void sort(List<WordFrequencyInfo> frequencyInfoList) {
-        frequencyInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
     }
 
     private List<String> splitWords(String inputString) {
         return new ArrayList<>(List.of(inputString.split(SPACE_DELIMITER)));
     }
 
-    private String generatePrintLines(List<WordFrequencyInfo> wordFrequencyInfoList) {
+    private String generateSortedPrintLines(List<WordFrequencyInfo> wordFrequencyInfoList) {
         return wordFrequencyInfoList.stream()
-                .map((wordFrequencyInfo) -> wordFrequencyInfo.getWord() + SPACE_CHAR + wordFrequencyInfo.getWordCount())
+                .sorted((wordFrequencyInfo1, wordFrequencyInfo2) -> wordFrequencyInfo2.getWordCount() - wordFrequencyInfo1.getWordCount())
+                .map(wordFrequencyInfo -> wordFrequencyInfo.getWord() + SPACE_CHAR + wordFrequencyInfo.getWordCount())
                 .collect(Collectors.joining(NEW_LINE_DELIMITER));
     }
 
